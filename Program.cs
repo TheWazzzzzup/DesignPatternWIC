3274011b8232a04954c250e6dd6edb742392449b
TheWazzzzzup <amitmoryosef@hotmail.com>
2022-11-29 17:26:20 +0200

    Init Setup

--- Program.cs
+++ Program.cs
@@ -1,32 +1,8 @@
-﻿// Shallow Copy Example
+﻿// Prototype Example
 class Program
 {
     static void Main(string[] args)
     {
-        Enemy yossi = new(health: 102,nickName: "YosBlet");
 
-        Enemy Liza = yossi;
-
-
-    }
-}
-
-class Enemy
-{
-    protected float damage;
-    protected string nickName;
-    protected int health;
-
-    public Enemy(int health, string nickName, float damage = 7 )
-    {
-        this.health = health;
-        this.damage = damage;
-        this.nickName = nickName;
-    }
-
-    public Enemy(Enemy copy)
-    {
-        this.health = copy.health;
-        this.nickName = copy.nickName;
     }
 }
