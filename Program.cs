08b63eeb5c82999e201dda47bba4d375c376d7ed
TheWazzzzzup <amitmoryosef@hotmail.com>
2022-11-29 17:24:38 +0200

    Class Example

--- Program.cs
+++ Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿// Shallow Copy Example
+class Program
+{
+    static void Main(string[] args)
+    {
+        Enemy yossi = new(health: 102,nickName: "YosBlet");
+
+        Enemy Liza = yossi;
+
+
+    }
+}
+
+class Enemy
+{
+    protected float damage;
+    protected string nickName;
+    protected int health;
+
+    public Enemy(int health, string nickName, float damage = 7 )
+    {
+        this.health = health;
+        this.damage = damage;
+        this.nickName = nickName;
+    }
+
+    public Enemy(Enemy copy)
+    {
+        this.health = copy.health;
+        this.nickName = copy.nickName;
+    }
+}
--- Singleton.cs
+++ Singleton.cs
@@ -1,34 +1,4 @@
 ﻿namespace DesignPatternWIC
 {
-    internal sealed class Account
-    {
-        private static Account _instance;
-        private static Object _lock;
 
-        private Account() { }
-
-        public static Account Instance()
-        {
-            lock (_lock)
-            {
-                if (_instance == null) { return _instance = new Account(); }
-                else return _instance;
-            }
-        }
-
-        public static void AccountAcsses()
-        {
-            Console.WriteLine("acsses some account");
-        }
-
-        public static void ChangePassword()
-        {
-            Console.WriteLine("change password");
-        }
-
-        public static void TwoStepsAut()
-        {
-            Console.WriteLine("activeate two steps authenticator");
-        }
-    }
 }
